33cbeaad7fea45c20e2bd2df10f730d2ea957ef4
Walid Chabane <walid.chabane@carestream.com>
2018-06-26 17:14:21 +0200

    WIP: Add dll main file

--- ProjectName/main.cpp
+++ ProjectName/main.cpp
@@ -1,4 +1,22 @@
-int main (int argc, char *argv[])
+#include <windows.h>
+
+#if 0 //May not needed
+typedef int(__stdcall* func_ptr_t)();//generic function pointer type
+#endif
+
+int main()
 {
-  return 0;
+   //Load DLL
+   auto dllHandle = LoadLibrary("AcquisitionStubDll.dll");
+   if (nullptr == dllHandle)
+   {
+      //Log : Cannot load "DllName" file
+      return EXIT_FAILURE;
+   }
+
+   //Resolve function address
+   GetProcAddress(dllHandle,"Exported_DLL_Func");
+
+   FreeLibrary(dllHandle);
+   return EXIT_SUCCESS;
 }
