4623dff3e4f32b14e13df755d891f30b6e7cf8fd
Walid Chabane <walid.chabane@carestream.com>
2018-06-27 17:16:27 +0200

    WIP
    WIP : code to

---
+++ ProjectName/DllName/DllSample.cpp
@@ -0,0 +1,11 @@
+#include "DllSample.h"
+
+int __cdecl samplefunc()
+{
+   return 1;
+}
+
+int __cdecl samplefunc2(int a)
+{
+   return a+1;
+}
--- ProjectName/main.cpp
+++ ProjectName/main.cpp
@@ -4,19 +4,44 @@
 typedef int(__stdcall* func_ptr_t)();//generic function pointer type
 #endif
 
+extern "C" int __cdecl samplefunc();
+extern "C" int __cdecl samplefunc2(int);
+
 int main()
 {
-   //Load DLL
-   auto dllHandle = LoadLibrary("AcquisitionStubDll.dll");
-   if (nullptr == dllHandle)
+   //https://www.tutorialspoint.com/dll/dll_quick_guide.htm
+
+
+   //DLL runtime dynamic linking
    {
-      //Log : Cannot load "DllName" file
-      return EXIT_FAILURE;
+      typedef int(__cdecl *SAMPLEFUNC)();
+      typedef int(__cdecl *SAMPLEFUNC2)(int);
+
+	   //Load DLL
+	   auto dllHandle = LoadLibrary("DllName.dll");
+	   if (nullptr == dllHandle)
+	   {
+	      //Log : Cannot load "DllName" file
+	      return EXIT_FAILURE;
+	   }
+	
+	   //Resolve function address
+	   SAMPLEFUNC samplefunc = (SAMPLEFUNC)GetProcAddress(dllHandle, "samplefunc");
+	   SAMPLEFUNC2 samplefunc2 = (SAMPLEFUNC2)GetProcAddress(dllHandle, "samplefunc2");
+	
+	   if (nullptr != samplefunc)
+	      auto ret1 = (samplefunc)();
+	
+	   if (nullptr != samplefunc2)
+	      auto ret2 = (samplefunc2)(5);
+	
+	   FreeLibrary(dllHandle);
    }
 
-   //Resolve function address
-   GetProcAddress(dllHandle,"Exported_DLL_Func");
-
-   FreeLibrary(dllHandle);
+   //DLL load time dynamic linking
+   {
+      auto ret1 = samplefunc();
+      auto ret2 = samplefunc2(5);
+   }
    return EXIT_SUCCESS;
 }
